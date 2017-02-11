a759e782f40f3ca9e45382628139795c39559cc0
Patrick Belliveau <patrick.t.belliveau@gmail.com>
2017-02-10 16:25:55 -0800

    fixed line continuation in init file

--- Scratch/__init__.py
+++ Scratch/__init__.py
@@ -2,7 +2,7 @@
 from .LineBuilder import LineBuilder
 from .ReadData import readMag, readTopo
 from .MagData import MagData
-from .DCIP import calcK, appRes, writeDCIP, writeDCObs2d, writeIPObs2d, makeTopo2d,
-                  makeMesh2d, makeWeights2d, writeDCInp2d, writeIPInp2d, readPredictedDatadc2d,
+from .DCIP import calcK, appRes, writeDCIP, writeDCObs2d, writeIPObs2d, makeTopo2d,\
+                  makeMesh2d, makeWeights2d, writeDCInp2d, writeIPInp2d, readPredictedDatadc2d,\
                   readObsDatadc2d
 from .MakeGrid import makeGrid, maskGrid
